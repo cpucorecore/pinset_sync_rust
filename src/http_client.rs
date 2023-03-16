9310a210c1d166a61058419bc48f7ca2db4a4ee8
cpucorecore <cpucorecore@gmail.com>
2023-03-16 11:39:21 +0800

    add http server functionality

--- src/http_client.rs
+++ src/http_client.rs
@@ -1,17 +1,16 @@
-pub async fn do_post() -> Result<String, reqwest::Error> {
+async fn do_post(url: &str) -> Result<String, reqwest::Error> {
     let client = reqwest::Client::new();
-    Ok(client
-        .post("http://192.168.0.85:5001/api/v0/repo/stat?size-only=false&human=false")
-        .send()
-        .await?
-        .text()
-        .await?)
+    Ok(client.post(url).send().await?.text().await?)
 }
 
 #[tokio::test]
 async fn test_do_post() {
-    match do_post().await {
+    match do_post(&"http://192.168.0.85:5001/api/v0/repo/stat?size-only=false&human=false").await {
         Ok(msg) => println!("do post response:{}", msg),
         Err(err) => println!("do post err:{}", err),
     };
+}
+
+pub async fn ipfs_repo_stat() -> Result<String, reqwest::Error> {
+    do_post(&"http://192.168.0.85:5001/api/v0/repo/stat?size-only=false&human=false").await
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,29 @@
-use log::info;
+use actix_web::{get, web, App, HttpServer, Responder};
+use pinset_sync_rust::http_client::ipfs_repo_stat;
+use std::time::Duration;
 
-fn main() {
+#[get("/ipfs_repo_stat")]
+async fn index() -> impl Responder {
+    match ipfs_repo_stat().await {
+        Ok(msg) => format!("do post response:{}", msg),
+        Err(err) => format!("do post err:{}", err),
+    }
+}
+
+#[get("/{name}")]
+async fn hello(name: web::Path<String>) -> impl Responder {
+    format!("hello {}!", &name)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
     log4rs::init_file("conf/log.yml", Default::default()).unwrap();
-    let _rt = tokio::runtime::Runtime::new().unwrap();
-    info!("main finished");
+
+    HttpServer::new(|| App::new().service(index).service(hello))
+        .workers(2)
+        .client_request_timeout(Duration::from_secs(30))
+        .keep_alive(Duration::from_secs(60))
+        .bind(("127.0.0.1", 8888))?
+        .run()
+        .await
 }
